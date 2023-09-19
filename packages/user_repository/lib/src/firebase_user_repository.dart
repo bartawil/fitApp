de8213e1631314b2dea70850ede0e23ca1691333
Bar <barrtawil@gmail.com>
2023-09-19 10:50:46 +0300

    user model

--- packages/user_repository/lib/src/firebase_user_repository.dart
+++ packages/user_repository/lib/src/firebase_user_repository.dart
@@ -1,5 +1,12 @@
 import 'package:user_repository/src/user_repo.dart';
 
 class FirebaseUserRepository implements UserRepository {
+  // Sign in
+
+  // Sign out
+
+  // Sign Up
+
+  // Reset Password
   
 }
