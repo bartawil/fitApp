20d64971684bd6c736f7fe4742758bdac9af29f4
Bar <barrtawil@gmail.com>
2023-09-19 16:49:10 +0300

    authentication screen

--- lib/app.dart
+++ lib/app.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_demo/blocs/authentication_bloc/authentication_bloc.dart';
+import 'package:user_repository/user_repository.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+import 'app_view.dart';
+
+class MainApp extends StatelessWidget {
+	final UserRepository userRepository;
+  const MainApp(this.userRepository, {super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(body: Center(child: Text("Hello"))),
-    );
+    return MultiRepositoryProvider(
+			providers: [
+				RepositoryProvider<AuthenticationBloc>(
+					create: (_) => AuthenticationBloc(
+						myUserRepository: userRepository
+					)
+				)
+			], 
+			child: const MyAppView()
+		);
   }
 }
