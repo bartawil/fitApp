0bfd913eb2d36728fb049bfed0f70a89f2525f89
Bar <barrtawil@gmail.com>
2023-09-19 10:50:52 +0300

    user model

--- packages/user_repository/lib/src/models/my_user.dart
+++ packages/user_repository/lib/src/models/my_user.dart
@@ -1,1 +1,67 @@
-class MyUser {}
+import 'dart:ui';
+
+import 'package:equatable/equatable.dart';
+import '../entities/entities.dart';
+
+class MyUser extends Equatable {
+  final String id;
+  final String email;
+  final String name;
+  final String? picture;
+
+  const MyUser({
+    required this.id, 
+    required this.email, 
+    required this.name, 
+    this.picture
+  });
+
+  // Empty user
+  static const empty = MyUser(
+    id: '', 
+    email: '', 
+    name: '',
+    picture: ''
+  );
+
+  // Change fields
+  MyUser copyWith ({
+    String? id,
+    String? email,
+    String? name,
+    String? picture,
+  }) {
+    return MyUser(
+      id:  id ?? this.id, 
+      email: email ?? this.email, 
+      name: name ?? this.name,
+      picture: picture ?? this.picture
+    );
+  }
+
+  bool get isEmpty => this == MyUser.empty;
+
+  bool get isNotEmpty => this != MyUser.empty;
+
+  MyUserEntity toEntity() {
+    return MyUserEntity(
+      id: id,
+      email: email,
+      name: name,
+      picture: picture,
+    );
+  }
+
+  static MyUser fromEntity(MyUserEntity entity) {
+    return MyUser(
+      id: entity.id, 
+      email: entity.email, 
+      name: entity.name,
+      picture: entity.picture,
+    );
+  }
+
+  @override
+  List<Object?> get props => [id, email, name, picture];
+
+}
