4ba1e2b51e3f4dfc26e05c4274739d48b61aa6d3
Bar <barrtawil@gmail.com>
2023-09-20 11:23:32 +0300

    firestore img

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -134,20 +134,65 @@ class _HomeScreenState extends State<HomeScreen> {
 																),
 															),
 													)
-												: Container(
-														width: 50,
-														height: 50,
-														decoration: BoxDecoration(
-															color: Colors.grey,
-															shape: BoxShape.circle,
-															image: DecorationImage(
-																image: NetworkImage(
-																	state.user!.picture!,
-																),
-																fit: BoxFit.cover
-															)
-														),
-													),
+												: GestureDetector(
+                          onTap: () async {
+															final ImagePicker picker = ImagePicker();
+															final XFile? image = await picker.pickImage(
+																source: ImageSource.gallery,
+																maxHeight: 500,
+																maxWidth: 500,
+																imageQuality: 40
+															);
+															if (image != null) {
+																CroppedFile? croppedFile = await ImageCropper().cropImage(
+																	sourcePath: image.path,
+																	aspectRatio: const CropAspectRatio(
+																		ratioX: 1, 
+																		ratioY: 1
+																	),
+																	aspectRatioPresets: [
+																		CropAspectRatioPreset.square
+																	],
+																	uiSettings: [
+																		AndroidUiSettings(
+																			toolbarTitle: 'Cropper',
+																			toolbarColor: Theme.of(context).colorScheme.primary,
+																			toolbarWidgetColor:Colors.white,
+																			initAspectRatio: CropAspectRatioPreset.original,
+																			lockAspectRatio: false
+																		),
+																		IOSUiSettings(
+																			title: 'Cropper',
+																		),
+																	],
+																);
+																if(croppedFile != null) {
+																	setState(() {
+																		context.read<UpdateUserInfoBloc>().add(
+																			UploadPicture(
+																				croppedFile.path,
+																				context.read<MyUserBloc>().state.user!.id
+																			)
+																		);
+																	});
+																}
+															}
+														},
+                            child: Container(
+                                width: 50,
+                                height: 50,
+                                decoration: BoxDecoration(
+                                  color: Colors.grey,
+                                  shape: BoxShape.circle,
+                                  image: DecorationImage(
+                                    image: NetworkImage(
+                                      state.user!.picture!,
+                                    ),
+                                    fit: BoxFit.cover
+                                  )
+                                ),
+                              ),
+                          ),
 											const SizedBox(width: 10),
 											Text(
 												"Welcome ${state.user!.name}"
