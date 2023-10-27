1dba470f3ade318940c620c20dca95f9ab1cd6e6
Bar <barrtawil@gmail.com>
2023-10-27 08:43:03 +0300

    dismissible

--- lib/components/weight_list.dart
+++ lib/components/weight_list.dart
@@ -27,73 +27,82 @@ class _WeightListState extends State<WeightList> {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Expanded(
-          child: ListView.builder(
-            itemCount: widget.weightList.length,
-            itemBuilder: (context, int i) {
-              return Padding(
-                padding: const EdgeInsets.symmetric(
-                    horizontal: 0.0, vertical: 1.0),
-                child: Slidable(
-                  endActionPane: ActionPane(
-                    extentRatio: 0.2,
-                    motion: const StretchMotion(),
-                    children: [
-                      SlidableAction(
-                        onPressed: (context) {
-                          if (latestWeight?.id == widget.weightList[i].id) {
-                            showDialog(
-                              context: context,
-                              builder: (context) {
-                                return const AlertDialog(
-                                  content: Text('You cannot delete the latest weight!'),
-                                );
-                              },
-                            );
-                          } else {
-                            context.read<WeightBloc>().add(DeleteWeight(widget.userId, widget.weightList[i].id));
-                          }
+          child: SlidableAutoCloseBehavior(
+            child: ListView.builder(
+              itemCount: widget.weightList.length,
+              itemBuilder: (context, int i) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 0.0, vertical: 1.0),
+                  child: Slidable(
+                    key: ValueKey(widget.weightList[i]),
+                    endActionPane: ActionPane(
+                      dismissible: DismissiblePane(
+                        onDismissed: () {
+                          context.read<WeightBloc>().add(DeleteWeight(widget.userId, widget.weightList[i].id));
                         },
-                        icon: Icons.delete,
-                        backgroundColor: Colors.red
-                      )
-                    ]
-                  ),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      border: Border(
-                        bottom: BorderSide(
-                          color: Colors.grey[300]!,
-                          width: 0.5,
+                      ),
+                      extentRatio: 0.2,
+                      motion: const StretchMotion(),
+                      children: [
+                        SlidableAction(
+                          onPressed: (context) {
+                            // if (latestWeight?.id == widget.weightList[i].id) {
+                            //   showDialog(
+                            //     context: context,
+                            //     builder: (context) {
+                            //       return const AlertDialog(
+                            //         content: Text('You cannot delete the latest weight!'),
+                            //       );
+                            //     },
+                            //   );
+                            // } else {
+                            //   context.read<WeightBloc>().add(DeleteWeight(widget.userId, widget.weightList[i].id));
+                            // }
+                            context.read<WeightBloc>().add(DeleteWeight(widget.userId, widget.weightList[i].id));
+                          },
+                          icon: Icons.delete,
+                          backgroundColor: Colors.red,
+                        )
+                      ]
+                    ),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        border: Border(
+                          bottom: BorderSide(
+                            color: Colors.grey[300]!,
+                            width: 0.5,
+                          ),
+                          right: BorderSide(
+                            color: Colors.grey[300]!,
+                            width: 0.5,
+                          ),
                         ),
-                        right: BorderSide(
-                          color: Colors.grey[300]!,
-                          width: 0.5,
+                      ),
+                      child: ListTile(
+                        contentPadding: const EdgeInsets.symmetric(
+                            horizontal: 16.0,
+                            vertical: 8.0), // Increase the vertical padding.
+                        title: Padding(
+                          padding: const EdgeInsets.only(
+                              bottom:
+                                  8.0), // Add padding to the bottom of the title.
+                          child: Text(
+                            '${widget.weightList[i].weight} kg',
+                            style: const TextStyle(fontSize: 18),
+                          ),
+                        ),
+                        trailing: Padding(
+                          padding: const EdgeInsets.only(left: 8.0),
+                          child: Text(DateFormat('dd.MM.yyyy')
+                              .format(widget.weightList[i].date)),
                         ),
                       ),
                     ),
-                    child: ListTile(
-                      contentPadding: const EdgeInsets.symmetric(
-                          horizontal: 16.0,
-                          vertical: 8.0), // Increase the vertical padding.
-                      title: Padding(
-                        padding: const EdgeInsets.only(
-                            bottom:
-                                8.0), // Add padding to the bottom of the title.
-                        child: Text(
-                          '${widget.weightList[i].weight} kg',
-                          style: const TextStyle(fontSize: 18),
-                        ),
-                      ),
-                      trailing: Padding(
-                        padding: const EdgeInsets.only(left: 8.0),
-                        child: Text(DateFormat('dd.MM.yyyy')
-                            .format(widget.weightList[i].date)),
-                      ),
-                    ),
                   ),
-                ),
-              );
-            },
+                );
+              },
+            ),
           ),
         ),
       ],
